58997b8ca60c29ce7643e84e5c3c06bee6056f18
Josileia <josileia.rodrigues@viacometa.com.br>
2022-05-10 16:17:10 -0400

    atualizacao incompleta de firebase

--- src/main.js
+++ src/main.js
@@ -5,12 +5,23 @@ import VMask from "v-mask";
 import Vuelidate from 'vuelidate';
 import VueRouter from 'vue-router';
 import Implementacoes from './components/Implementacoes.vue';
-import FirebaseVue from './firebase';
+import { initializeApp }from "firebase/app";
 
 
 Vue.use(Vuelidate);
 Vue.use(VueRouter);
-Vue.use(FirebaseVue);
+
+initializeApp({
+  apiKey: "AIzaSyCftxJfKLda--unY5EJ5nboAeFkwa4D2uM",
+  authDomain: "project-vuetify-458e1.firebaseapp.com",
+  projectId : "project-vuetify-458e1",
+  storageBucket:"project-vuetify-458e1.appspot.com",
+  messagingSenderId: "867605529478",
+  appId: "1:867605529478:web:fadd646574305366f76207",
+  measurementId:"G-ZBFYLR5W2Z"
+
+})
+
 const router = new VueRouter({
   routes:[
     {
